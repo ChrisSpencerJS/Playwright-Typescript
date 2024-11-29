a4c8d4e88f963cb9c489a729670d5d2d73b7ff05
Chris Spencer <32935122+ChrisSpencerJS@users.noreply.github.com>
2024-11-29 18:07:10 +0000

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.google.com/',
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
